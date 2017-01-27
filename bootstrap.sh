7aa5944da9318706d7189add3d96f669900b2d64
Reedyseth <reedyseth@gmail.com>
2017-01-26 21:51:08 -0700

    Add more provisioning software

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 apt-get update
-
+# Installing Apache
 apt-get install -y apache2
 
 if ! [ -L /var/www ]; then
 	rm -rf /var/www
 	ln -sf /vagrant /var/www
 fi
+# Installing PHP
+apt-get install -y php5 php5-mysql php5-xdebug
+# Installing MySQL
+apt-get install -y mysql-server-5.5
+# Installing phpMyAdmin
+apt-get install -y phpmyadmin
+# Installing git
+apt-get install -y git
+# Retrieving WordPress via Git, it is faster to retrieve the ZIP
+git clone https://github.com/WordPress/WordPress.git /var/www/wordpress
+
